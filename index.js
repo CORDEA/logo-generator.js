dbbedd5bb1a0859ccfa8e9ae5799cf16600bff6f
Yoshihiro Tanaka <contact@cordea.jp>
2020-01-04 13:41:32 +0900

    update background color

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const MDCTextField = mdc.textField.MDCTextField;
 const MDCRipple = mdc.ripple.MDCRipple;
 
-const foregroundTextField = new MDCTextField(document.querySelector('.foreground'));
-const backgroundTextField = new MDCTextField(document.querySelector('.background'));
+const foregroundTextField = new MDCTextField(document.querySelector('.foreground-text-field'));
+const backgroundTextField = new MDCTextField(document.querySelector('.background-text-field'));
 new MDCRipple(document.querySelector('.mdc-button'));
 
 foregroundTextField.value = "#00000000";
 backgroundTextField.value = "#00FFFFFF";
 
 function onClick() {
+    const background = document.getElementsByClassName("background");
+    const backgroundColor = backgroundTextField.value;
+    for (let i = 0; i < background.length; i++) {
+        background[i].setAttribute("fill", backgroundColor);
+    }
     foregroundTextField.value;
-    backgroundTextField.value;
 }
