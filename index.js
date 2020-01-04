84cf69a646a3b0b438fb26d0c4ba64a7df6129b7
Yoshihiro Tanaka <contact@cordea.jp>
2020-01-04 12:37:15 +0900

    add button

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const MDCTextField = mdc.textField.MDCTextField;
+const MDCRipple = mdc.ripple.MDCRipple;
 
 new MDCTextField(document.querySelector('.foreground'));
 new MDCTextField(document.querySelector('.background'));
+new MDCRipple(document.querySelector('.mdc-button'));
