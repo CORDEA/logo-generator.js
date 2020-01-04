32be20b8cd6045aef650e15b91b9b8838351bf7e
Yoshihiro Tanaka <contact@cordea.jp>
2020-01-04 12:44:09 +0900

    set default value

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const MDCTextField = mdc.textField.MDCTextField;
 const MDCRipple = mdc.ripple.MDCRipple;
 
-new MDCTextField(document.querySelector('.foreground'));
-new MDCTextField(document.querySelector('.background'));
+const foregroundTextField = new MDCTextField(document.querySelector('.foreground'));
+const backgroundTextField = new MDCTextField(document.querySelector('.background'));
 new MDCRipple(document.querySelector('.mdc-button'));
+
+foregroundTextField.value = "#00000000";
+backgroundTextField.value = "#00FFFFFF";
